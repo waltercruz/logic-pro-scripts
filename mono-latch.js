6010bc5e4cf273ed5bfc5d882ae540aeda2414a3
Walter Cruz <waltercruz@gmail.com>
2021-09-12 13:37:42 -0300

    fix: Cleaning up

--- mono-latch.js
+++ mono-latch.js
@@ -1,27 +1,20 @@
 
 function HandleMIDI(e) {
     // Trace(e)
-    if (e instanceof NoteOn && e.pitch != GetParameter('Unlatch Note')) {
+    if (e instanceof NoteOn) {
         const notes = [...Array(128).keys()]
         notes.forEach(function (noteNumber) {
-            f = new NoteOff();
+            let f = new NoteOff();
             f.pitch = noteNumber;
             f.velocity = e.velocity;
             //Trace(f)
             f.send();
         })
-        Trace(e)
-        e.send();
-    }
-    if (e.pitch == GetParameter('Unlatch Note')) {
-        const notes = [...Array(128).keys()]
-        notes.forEach(function (noteNumber) {
-            f = new NoteOff();
-            f.pitch = noteNumber;
-            f.velocity = e.velocity;
-            //Trace(f)
-            f.send();
-        })
+        if (e.pitch != GetParameter('Unlatch Note')) {
+            // Trace(e)
+            e.send();
+        }
+
     }
     return;
 }
